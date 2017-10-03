ac2340a1f1c9d22ce2a6194179687bb3e3ae7a53
Nic Jackson <jackson.nic@gmail.com>
2017-10-03 18:50:29 +0100

    Removed dependencies

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "KituraHTTPTest"
-/*    
+    name: "KituraHTTPTest",
+    /*
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "1.7.0"),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "16.0.0")
     ],
+    */
     
     targets: [
         .target(name: "KituraHTTPTest", dependencies: ["KituraNet", "SwiftyJSON"]),
         .testTarget(name: "KituraHTTPTestTests", dependencies: ["KituraHTTPTest"])
     ]
-*/
 )
