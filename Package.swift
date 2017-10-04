54f1cc3f53ebd7c9607c717a089a5264daacd67c
Nic Jackson <jackson.nic@gmail.com>
2017-10-04 09:13:31 +0100

    Added library support

--- Package.swift
+++ Package.swift
@@ -3,10 +3,17 @@ import PackageDescription
 
 let package = Package(
     name: "KituraHTTPTest",
+
+    products: [
+        .library(
+            name: "KituraHTTPTest",
+            targets: ["KituraHTTPTest"]
+        )
+    ],
     
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "1.7.0"),
-        .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "16.0.0")
+        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", .upToNextMinor(from: "1.7.0")),
+        .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", .upToNextMinor(from: "17.0.0"))
     ],
     
     targets: [
