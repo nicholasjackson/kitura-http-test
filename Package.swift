93e0462c9fee3343c61f072db1436b3df29fc22e
Nic Jackson <jackson.nic@gmail.com>
2017-10-04 09:04:35 +0100

    Restored Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "KituraHTTPTest",
     
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", .branch("master")),
+        .package(url: "https://github.com/IBM-Swift/Kitura-net.git", from: "1.7.0"),
         .package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "16.0.0")
     ],
     
